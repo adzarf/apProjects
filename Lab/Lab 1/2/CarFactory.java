09664ebe3547c8f4ddb6976f7a614355d3f53718
adzarf <aqildzarfan@protonmail.com>
2025-04-08 12:22:02 +0800

    Made changes to CarFactory

--- Lab/Lab 1/2/CarFactory.java
+++ Lab/Lab 1/2/CarFactory.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 public class CarFactory {
     Scanner in = new Scanner(System.in);
-    Car car = new Car();
 
-    public void buildCar() {
+    public Car buildCar() {
+        Car car = new Car();
+
         System.out.println("Enter model: ");
         car.setModel(in.nextLine());
 
         System.out.println("Enter Horse Power: ");
         car.setHorsePower(in.nextInt());
+
+        return car;
     }
 
     public void modifyCar(Car obj, String model, int horsePower) {
